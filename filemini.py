a5e294a0751cfb19b7ea58b70db9bec3e6303df4
hungnm04 <ngomanhhung40@gmail.com>
2026-02-25 22:14:42 +0700

    Final files uploaded

--- filemini.py
+++ filemini.py
@@ -12,26 +12,36 @@ def ipds_window_manipulate(ipds: pd.Series, window: int):
     ipds = pd.Series(window_mean)
     return ipds
 
-def get_raw_ipds(filedir: str, filename: str, row_range: tuple = None, window: int = None, value_range: tuple = (1, 99)):
-    # This method reads the csv file "filedir + filename + ".csv" and return all the IPDs it contains.
-    ipds = pd.read_csv(filedir + filename + ".csv")["IPDs"].dropna()
-    if value_range != None:
-        up = ipds.quantile(q = value_range[1] / 100)
-        down = ipds.quantile(q = value_range[0] / 100)
-        ipds = ipds[ipds[ipds >= down].index].reset_index(drop = True)
-        ipds = ipds[ipds[ipds <= up].index].reset_index(drop = True)
+def get_raw_ipds(filedir: str, filename: str, row_range: tuple = None,
+                 window: int = None, value_range: tuple = (1, 99)):
 
-    if row_range != None:
+    # This method reads the csv file and returns all the IPDs it contains.
+    path = filedir + filename + ".csv"
+
+    try:
+        ipds = pd.read_csv(path, encoding="utf-8")["IPDs"].dropna()
+    except UnicodeDecodeError:
+        ipds = pd.read_csv(path, encoding="utf-16")["IPDs"].dropna()
+
+    if value_range is not None:
+        up = ipds.quantile(q=value_range[1] / 100)
+        down = ipds.quantile(q=value_range[0] / 100)
+        ipds = ipds[ipds[ipds >= down].index].reset_index(drop=True)
+        ipds = ipds[ipds[ipds <= up].index].reset_index(drop=True)
+
+    if row_range is not None:
         assert row_range[0] >= 0 and row_range[1] >= 0
         if row_range[1] > len(ipds):
             row_range = (row_range[0], len(ipds))
         ipds = ipds[row_range[0]: row_range[1]]
-    print("IPDs filepath:", filedir + filename + ".csv")
+
+    print("IPDs filepath:", path)
     print("IPDs count:", len(ipds))
     print("IPDs range:", row_range)
-    # normally do not need
-    if window != None:
-        ipds = ipds_window_manipulate(ipds = ipds, window = window)
+
+    if window is not None:
+        ipds = ipds_window_manipulate(ipds=ipds, window=window)
+
     return ipds
 
 def get_discretized_parameters(filedir: str, filename: str):
